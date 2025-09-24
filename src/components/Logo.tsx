24349709f5cc576196afdae87023392367dd630d
Soulemane12 <16soulemanesow@gmail.com>
2025-09-23 22:41:19 -0400

    big

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 import Image from 'next/image'
 
 interface LogoProps {
-  size?: 'sm' | 'md' | 'lg'
+  size?: 'sm' | 'md' | 'lg' | 'xl'
   className?: string
 }
 
 export default function Logo({ size = 'md', className = '' }: LogoProps) {
   const sizeClasses = {
     sm: 'h-8 w-auto',
-    md: 'h-12 w-auto', 
-    lg: 'h-16 w-auto'
+    md: 'h-12 w-auto',
+    lg: 'h-16 w-auto',
+    xl: 'h-24 w-auto'
   }
 
   return (
@@ -18,8 +19,8 @@ export default function Logo({ size = 'md', className = '' }: LogoProps) {
       <Image
         src="/eventinsta_logo.png"
         alt="EventsInsta"
-        width={size === 'sm' ? 32 : size === 'md' ? 48 : 64}
-        height={size === 'sm' ? 32 : size === 'md' ? 48 : 64}
+        width={size === 'sm' ? 32 : size === 'md' ? 48 : size === 'lg' ? 64 : 96}
+        height={size === 'sm' ? 32 : size === 'md' ? 48 : size === 'lg' ? 64 : 96}
         className={`${sizeClasses[size]} -mt-3`}
         priority
       />
